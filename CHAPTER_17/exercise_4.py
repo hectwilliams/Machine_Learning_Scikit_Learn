455d36cbd3b0ca2559461b648012719f9ec24d26
Hector <hectwilliams@gmail.com>
2025-02-27 17:38:16 -0500

    fix error in exercise 4

--- CHAPTER_17/exercise_4.py
+++ CHAPTER_17/exercise_4.py
@@ -6,6 +6,13 @@ Q.
 
 A.
     Undercomplete and overcomplete are the dimensions of the codings. Undercomplete autoencoders has a lower dimensionality and vice versa for overcomplete. 
+    
     The risk of excessively undercomplete autoencoders is reducing dimensionality to a point where the network can't learn much : if you lose 4 out of 5 senses can you really learn much
-    Overcomplete autoencoders may produce bad 'codings' as the network won't have work hard to discover patterns: similar to an overfitting model because of its large number of dimensions.
+    
+    The risk of overcomplete autoencoders is only simple patterns will be learned: to many senses will diminish learning as the network will be streteched to thin and each sense will only need to learn simple patterns as other senses will help support. 
+    Learneing simple patterns about one image cannot be used to evaluate other images of varying complexity.  
+
+     
+    
 """
+
