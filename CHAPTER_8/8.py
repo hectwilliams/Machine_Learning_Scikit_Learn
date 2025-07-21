89ced2b06550f3891f42696495370148ffff5a4b
Hector <hectwilliams@gmail.com>
2025-07-20 21:25:06 -0400

    dim -reduc

---
+++ CHAPTER_8/8.py
@@ -0,0 +1,16 @@
+'''
+
+  Q. 
+
+    Does it make sense to chain two different dimensionality reduction algorithms?
+
+  A.
+
+    Yes, some algorithms may be good at clustering while another may be good at finding classes which are farthest apart. Combination of those would produce an algorithm with good seperation between d-reduced set.
+
+    Some algorithms are better are filtering outlier or useless data. 
+
+    Combining algorithms may speed up processing time (filter outlier d-reduce algorithm followed by a clusting perfomant d-reduce algorithm  )
+
+
+'''
